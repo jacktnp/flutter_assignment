c8fcace1a95c5bce22f0acb6e10f04d7c7653b9b
jacktnp <thanaponpkw@gmail.com>
2019-02-17 03:31:46 +0700

    :coffee: code

---
+++ lib/ui/home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return HomePageState();
+  }
+
+}
+
+
+class HomePageState extends State<HomePage> {
+
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+      Text('Home'),
+      Text('Notify'),
+      Text('Map'),
+      Text('Profile'),
+      Text('Setup')
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
+      ),
+      
+      body: Center(
+        child:_children[_currentIndex]
+      ), 
+
+      bottomNavigationBar:new Theme(
+        data: Theme.of(context).copyWith(
+        canvasColor: Colors.blue,
+      ),
+        
+        child: new BottomNavigationBar(
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            }
+          );
+        },
+       
+       currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem( 
+            icon: Icon(Icons.dashboard),
+            title: Text("Home")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text("Notify")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            title: Text("map")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Profile")
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("Setup")
+          ),
+        ],
+
+      ),
+    ),
+
+    );
+  }
+}
---
+++ lib/ui/register.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class RegisterForm extends StatefulWidget{
+  @override
+  MyCustomFormState createState(){
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<RegisterForm>{
+  final _formKey = GlobalKey<FormState>();
+  var password_1 = '';
+  var password_2 = '';
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 30, right: 20, left: 20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Email",
+                  hintText: "Email",
+                  icon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  if(value.isEmpty){
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                  if(value.toLowerCase() == "admin"){
+                    return "user นี้มีอยู่ในระบบแล้ว";
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Password",
+                  hintText: "Password",
+                  icon: Icon(Icons.lock),
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  this.password_1 = value;
+                  if(value.isEmpty){
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Password",
+                  hintText: "Password",
+                  icon: Icon(Icons.lock),
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  this.password_1 = value;
+                  if(value.isEmpty){
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                  if(password_1 != password_2){
+                    return "รหัสผ่านไม่ตรงกัน";
+                  }
+                },
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: RaisedButton(
+                    child: Text("CONTINUE"),
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        Navigator.pushNamed(context, '/');
+                      }
+                    },
+                  ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
